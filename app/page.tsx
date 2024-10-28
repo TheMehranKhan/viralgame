66b053d6d3e2bc49d42d5514e951ad9bf2f84aea
Mehran <khanmohammadimehran@gmail.com>
2024-10-28 12:34:36 +0000

    GenX update

--- app/page.tsx
+++ app/page.tsx
@@ -63,25 +63,11 @@ export default async function Home() {
           style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
         >
           <a
-            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
+            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-lime bg-lime px-5 py-2 text-sm text-black transition-colors hover:bg-black hover:text-white"
             href={DEPLOY_URL}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
-              className="h-4 w-4 group-hover:text-black"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 4L20 20H4L12 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
             <p>Download for Android</p>
           </a>
           <a
